4eb49dde80fbe2ed7187c086b61772a68d0d4d59
varun7797 <varun7797@gmail.com>
2019-10-24 13:03:56 -0500

    mod

---
+++ ez-cal/src/firebase.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZzj4QwsGSpJmXRiVjuqgAq-5YB9EoxrE",
+  authDomain: "ezcal-2394a.firebaseapp.com",
+  databaseURL: "https://ezcal-2394a.firebaseio.com",
+  projectId: "ezcal-2394a",
+  storageBucket: "ezcal-2394a.appspot.com",
+  messagingSenderId: "1029216905931",
+  appId: "1:1029216905931:web:33ccd473548edd99ecc94e"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false
+  }
+};
+
+export const db = firebase.database().ref("users");
