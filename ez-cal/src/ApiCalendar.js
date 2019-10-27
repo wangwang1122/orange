d82964c9996b467c5cc050257f1392b65637362d
Sarah Ahmad <sarahahmad2019@gmail.com>
2019-10-27 17:10:02 -0500

    moved calendar rendering into ApiCalendar

--- ez-cal/src/ApiCalendar.js
+++ ez-cal/src/ApiCalendar.js
@@ -4,6 +4,7 @@ import App from './App'
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import LinkGenerator from "./LinkGenerator";
+import { Switch, Route } from 'react-router-dom'
 
 const Config = require("./data/apiGoogleconfig.json");
 
@@ -40,6 +41,82 @@ export default class ApiCalendar extends Component  {
             console.log(e);
         }
     }
+    buildDayList = () => {
+        let date = new Date();
+        let weekDays = ["Su", "M", "Tu", "W", "Th", "F", "Sa"];
+        let dayList = [], dateList = [];
+    
+        for (let i = 0; i < 7; i++) {
+          dayList.push(weekDays[(date.getDay() + i) % 7]);
+          dateList.push(date.getDate() + i);
+        }
+    
+        return [dayList, dateList];
+      };
+    
+    dayGrid = () => {
+        let [days, dates] = this.buildDayList();
+        let timeBlock = subTimes.map(x => {
+          return x.split(":")[1] === "00" ? <div className="dayTime">{x}</div> : <div className="dayTime"></div>;
+        });
+    
+        let cal = [];
+        cal.push(<div className="dayCol">
+          <div className="dayEmpty"></div>
+          {timeBlock}
+        </div>
+        );
+    
+        for (let i = 0; i < days.length; i++) {
+          let hours = [];
+          for (let j = 0; j < subTimes.length; j++) {
+            if (times.includes(subTimes[j])) {
+              hours.push(<div id={`${dates[i]} ${subTimes[j]}`} className="dayHour" />);
+            } else {
+              hours.push(<div id={`${dates[i]} ${subTimes[j]}`} className="daySubHour" />);
+            }
+          }
+    
+          cal.push(<div className="dayCol">
+            <div className="dayHead">{days[i]}
+              <div className="dayDate">{dates[i]}</div>
+            </div>
+            {hours}
+          </div>);
+        }
+    
+        return (
+          <div className="dayWrapper">
+            {cal}
+          </div>
+        );
+      };
+
+      SharedCalendar = () => {
+        return (
+          <div>
+            {this.dayGrid()}
+          </div>
+        );
+      };
+    
+      MyCalendar = () => {
+        return (
+          <div>
+            {this.dayGrid()}
+          </div>
+        );
+      };
+    
+      Main = () => {
+        return (
+          <Switch>
+            <Route exact path='/' component={this.MyCalendar} />
+            <Route path={window.location.pathname} component={this.SharedCalendar} />
+          </Switch>
+        );
+      };
+    
     componentDidMount = () => {
         const userLink = window.location.pathname.substring(1);
         if (userLink) {
@@ -389,6 +466,7 @@ export default class ApiCalendar extends Component  {
                     {this.state.uid ? <div>Welcome, {this.state.userName}!</div> : <button onClick={() => { this.Login()  }}>Sync with Google</button>}
                     {this.state.uid ? <LinkGenerator link={this.setLink(this.state.uid)} /> : null}
 
+                    {this.Main()}
             </div>
             )
     }
--- ez-cal/src/App.js
+++ ez-cal/src/App.js
@@ -13,90 +13,11 @@ export default class App extends Component {
     super(props);
   };
 
-  buildDayList = () => {
-    let date = new Date();
-    let weekDays = ["Su", "M", "Tu", "W", "Th", "F", "Sa"];
-    let dayList = [], dateList = [];
-
-    for (let i = 0; i < 7; i++) {
-      dayList.push(weekDays[(date.getDay() + i) % 7]);
-      dateList.push(date.getDate() + i);
-    }
-
-    return [dayList, dateList];
-  };
-
-  dayGrid = () => {
-    let [days, dates] = this.buildDayList();
-    let timeBlock = subTimes.map(x => {
-      return x.split(":")[1] === "00" ? <div className="dayTime">{x}</div> : <div className="dayTime"></div>;
-    });
-
-    let cal = [];
-    cal.push(<div className="dayCol">
-      <div className="dayEmpty"></div>
-      {timeBlock}
-    </div>
-    );
-
-    for (let i = 0; i < days.length; i++) {
-      let hours = [];
-      for (let j = 0; j < subTimes.length; j++) {
-        if (times.includes(subTimes[j])) {
-          hours.push(<div id={`${dates[i]} ${subTimes[j]}`} className="dayHour" />);
-        } else {
-          hours.push(<div id={`${dates[i]} ${subTimes[j]}`} className="daySubHour" />);
-        }
-      }
-
-      cal.push(<div className="dayCol">
-        <div className="dayHead">{days[i]}
-          <div className="dayDate">{dates[i]}</div>
-        </div>
-        {hours}
-      </div>);
-    }
-
-    return (
-      <div className="dayWrapper">
-        {cal}
-      </div>
-    );
-  };
-
-  SharedCalendar = () => {
-    return (
-      <div>
-        {this.dayGrid()}
-      </div>
-    );
-  };
-
-  MyCalendar = () => {
-    return (
-      <div>
-        {this.dayGrid()}
-      </div>
-    );
-  };
-
-  Main = () => {
-    return (
-      <Switch>
-        <Route exact path='/' component={this.MyCalendar} />
-        <Route path={window.location.pathname} component={this.SharedCalendar} />
-      </Switch>
-    );
-  };
-
   render() {
     return (
       <div className="container">
         <ApiCalendar/>
-
-        {this.Main()}
         <Addevents />
-
       </div>
     );
   }
